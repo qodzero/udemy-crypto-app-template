b8c6098114d1ea1d833f0dd7f5d86fc696e0a897
Samuel Mthembo <fourkay537@gmail.com>
2022-04-13 13:33:49 +0200

    Fixed iconfonts registration

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app import MainApp
 # Register icons to kivy font register
 register(
     "Feather",
-    join(dirname(__file__, 'assets/fonts/feather/feather.ttf')),
-    join(dirname(__file__, 'assets/fonts/feather/feather.fontd')),
+    join(dirname(__file__), 'assets/fonts/feather/feather.ttf'),
+    join(dirname(__file__), 'assets/fonts/feather/feather.fontd'),
 )
 MainApp().run()
